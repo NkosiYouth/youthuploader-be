243cb63eb10dcd120ab4398ca0b1c67d6022b1f2
ahmad_bigxdata <ahmad@bigxdata.io>
2024-04-17 07:53:25 +0500

    Increased Memory Limit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     DEBUG = True
     MONGO_URI = 'mongodb+srv://yawit:XOyeQIFUInP8cwC3@cluster0.ashuhhm.mongodb.net/youtuh_data?retryWrites=true&w=majority&appName=Cluster0'
-
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 512 MB
