f71ba6516e391a860964b7a06136ef44b17b1506
NkosiYouth <nkosi@sayouthatwork.com>
2024-04-22 08:12:10 +0200

    hf url update

--- scripts/ai_script.py
+++ scripts/ai_script.py
@@ -152,7 +152,7 @@ def ai_model(file_path, file_name, cohort):
     hf_token = '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'
 
     # Initialize the Gradio client with your Hugging Face token
-    client = Client("https://nkosiyouth-ocr-image-to-text.hf.space/--replicas/0prxu/", hf_token=hf_token)
+    client = Client("https://nkosiyouth-ocr-image-to-text.hf.space/--replicas/sszfq/", hf_token=hf_token)
 
     results_list = []
 
