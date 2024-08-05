8b64f00c963363743a926dda5f01ef607b6c6d78
NkosiYouth <nkosi@sayouthatwork.com>
2024-08-05 11:58:58 +0200

    Update image_text_model.py

--- scripts/image_text_model.py
+++ scripts/image_text_model.py
@@ -10,7 +10,7 @@ def image_text_model(image_file_paths):
     hf_token = os.getenv('HFT')
 
     # Initialize the Gradio client with your Hugging Face token
-    client = Client("https://nkosiyouth-ocr-image-to-text.hf.space/--replicas/bceuh/", hf_token=hf_token)
+    client = Client("https://nkosiyouth-ocr-image-to-text-old-version.hf.space/--replicas/d5vvl/", hf_token=hf_token)
 
     results_list = []
 
